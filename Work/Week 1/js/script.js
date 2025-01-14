6cedff160374aecb2c5d932dfca7f8634fe78b1e
Biwanka <gauthier.bibi10@gmail.com>
2025-01-14 15:31:42 -0500

    doing the taks in class

--- Work/Week 1/js/script.js
+++ Work/Week 1/js/script.js
@@ -2,9 +2,28 @@
 
 function setup() {
     console.log("go")
-
+    createCanvas(900, 400);
 }
 
 function draw() {
+    background("black");
+
+    //ellipse 1
+    push();
+    fill(235, 52, 116);
+    ellipse(40, 50, 35, 35);
+    pop();
+
+    //ellipse 2
+    push();
+    fill(168, 50, 92);
+    ellipse(100, 110, 55, 55);
+    pop();
+
+    //ellipse 1
+    push();
+    fill(125, 16, 60);
+    ellipse(160, 180, 65, 65);
+    pop();
 
 }
