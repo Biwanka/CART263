fa8fffaed4cdd29bc6bd984a7caf6b1c2249ae28
Biwanka <gauthier.bibi10@gmail.com>
2025-03-04 10:36:44 -0500

    resized the image of the character. also added all of the png
    may need to redraw the character, as i drew them to big and scalling them down they become blurry

--- Work/Maze_Game/p5-rev-template/js/script.js
+++ Work/Maze_Game/p5-rev-template/js/script.js
@@ -9,10 +9,23 @@ let characterFrontWalk = [
 
 let characterBackWalk = [
     {
-        imageLegLeft: undefined,
-        imageLegRight: undefined,
+        imageRightLeg: undefined,
+        imageLeftLeg: undefined,
     }
 ]
+
+let characterStill = {
+
+    x: undefined,
+    y: undefined,
+    height: 55,
+    width: 40,
+    image: {
+        front: undefined,
+        back: undefined,
+    }
+
+}
 
 const rightWall = {
     x: undefined,
@@ -46,6 +59,12 @@ let gameBackdrop = {
 function preload() {
     longHallway.image = loadImage("assets/images/trial.png");
     gameBackdrop.image = loadImage("assets/images/Paper_Backdrop.png");
+    characterFrontWalk.imageRightLeg = loadImage("assets/images/Character_Front_Right.png");
+    characterFrontWalk.imageLeftLeg = loadImage("assets/images/Character_Front_Left.png");
+    characterBackWalk.imageRightLeg = loadImage("assets/images/Character_Back_Right.png");
+    characterBackWalk.imageLeftLeg = loadImage("assets/images/Character_Back_Left.png");
+    characterStill.image.front = loadImage("assets/images/Character_Front.png");
+    characterStill.image.back = loadImage("assets/images/Character_Back.png");
 }
 
 function setup() {
@@ -53,7 +72,7 @@ function setup() {
     createCanvas(1000, 650);
     background(gameBackdrop.image);
 
-
+    characterStill.image.front.resize(75, 80);
 
 }
 
@@ -67,6 +86,16 @@ function draw() {
 }
 
 
+function drawCharacter() {
+
+    push();
+    rectMode(CENTER);
+    imageMode(CENTER);
+    rect(100, 100, characterStill.width, characterStill.height);
+    image(characterStill.image.front, 100, 100);
+    pop();
+
+}
 
 
 function drawLongHallway() {
