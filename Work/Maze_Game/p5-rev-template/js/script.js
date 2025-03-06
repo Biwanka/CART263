3921883355981be5497ffd2b7d205bd0e5ed5ecf
Biwanka <gauthier.bibi10@gmail.com>
2025-03-05 21:13:55 -0500

    got the character to walk.
    though the else statement dosent work. the character should go back to the still image when the key is not down. need to figure the problem out

--- Work/Maze_Game/p5-rev-template/js/script.js
+++ Work/Maze_Game/p5-rev-template/js/script.js
@@ -1,31 +1,43 @@
 "use strict";
 
-let characterFrontWalk = [
-    {
-        imageRightLeg: undefined,
-        imageLeftLeg: undefined,
-    }
-]
 
-let characterBackWalk = [
-    {
-        imageRightLeg: undefined,
-        imageLeftLeg: undefined,
-    }
-]
+// let characterBackWalk = [
+//     {
 
-let characterStill = {
+//     }
+// ]
 
-    x: undefined,
-    y: undefined,
+const characterWalk = {
+
+    x: 100,
+    y: 100,
     height: 55,
     width: 40,
-    image: {
-        front: undefined,
-        back: undefined,
-    }
+    imageFront: undefined,
+    imageBack: undefined,
+
+    front: {
+        imageRightLeg: undefined,
+        imageMiddle: undefined,
+        imageLeftLeg: undefined,
+    },
+
+    back: {
+        imageRightLeg: undefined,
+        imageMiddle: undefined,
+        imageLeftLeg: undefined,
+    },
+
+    //     side: {
+    // image
+    //     }
 
 }
+
+// const characterFrontWalk = {
+
+
+// }
 
 const longHallway = {
 
@@ -38,16 +50,16 @@ const longHallway = {
     },
 
     rigthWall: {
-        x: longHallway.floor.x + 5,
-        y: longHallway.floor.y,
+        x: undefined,//longHallway.floor.x + 5,
+        y: undefined,//longHallway.floor.y,
         width: 30,
         height: 600,
         image: undefined
     },
 
     leftWall: {
-        x: longHallway.floor.x - 5,
-        y: longHallway.floor.y,
+        x: undefined, //longHallway.floor.x - 5,
+        y: undefined,//longHallway.floor.y,
         width: 30,
         height: 600,
         image: undefined
@@ -55,37 +67,130 @@ const longHallway = {
 
 }
 
+let legPosition = 0;
+let isMoving = false;
+
 let gameBackdrop = {
     image: undefined,
 }
 
+
 function preload() {
-    longHallway.image = loadImage("assets/images/trial.png");
+    longHallway.floor.image = loadImage("assets/images/trial.png");
     gameBackdrop.image = loadImage("assets/images/Paper_Backdrop.png");
-    characterFrontWalk.imageRightLeg = loadImage("assets/images/Character_Front_Right.png");
-    characterFrontWalk.imageLeftLeg = loadImage("assets/images/Character_Front_Left.png");
-    characterBackWalk.imageRightLeg = loadImage("assets/images/Character_Back_Right.png");
-    characterBackWalk.imageLeftLeg = loadImage("assets/images/Character_Back_Left.png");
-    characterStill.image.front = loadImage("assets/images/Character_Front.png");
-    characterStill.image.back = loadImage("assets/images/Character_Back.png");
+
+    characterWalk.imageFront = loadImage("assets/images/Character_Front.png");
+    characterWalk.imageBack = loadImage("assets/images/Character_Back.png");
+
+    characterWalk.front.imageRightLeg = loadImage("assets/images/Character_Front_Right.png");
+    characterWalk.front.imageMiddle = loadImage("assets/images/Character_Front_Middle.png");
+    characterWalk.front.imageLeftLeg = loadImage("assets/images/Character_Front_Left.png");
+
+    characterWalk.back.imageRightLeg = loadImage("assets/images/Character_Back_Right.png");
+    characterWalk.back.imageMiddle = loadImage("assets/images/Character_Back_Middle.png");
+    characterWalk.back.imageLeftLeg = loadImage("assets/images/Character_Back_Left.png");
+
+    // characterFrontWalk.imageRightLeg = loadImage("assets/images/Character_Front_Right.png");
+    // characterFrontWalk.imageLeftLeg = loadImage("assets/images/Character_Front_Left.png");
+    // characterFrontWalk.imageMiddle = loadImage("assets/images/Character_Front_Middle.png");
+
+    // characterBackWalk.imageRightLeg = loadImage("assets/images/Character_Back_Right.png");
+    // characterBackWalk.imageLeftLeg = loadImage("assets/images/Character_Back_Left.png");
+
+    // characterStill.imageFront = loadImage("assets/images/Character_Front.png");
+    // characterStill.imageBack = loadImage("assets/images/Character_Back.png");
 }
 
 function setup() {
     console.log("go")
     createCanvas(1000, 650);
-    background(gameBackdrop.image);
 
-    characterStill.image.front.resize(75, 80);
-    longHallway.image.resize(150, 600);
+
+    characterWalk.imageFront.resize(75, 80);
+    characterWalk.imageBack.resize(75, 80);
+
+    characterWalk.front.imageRightLeg.resize(75, 80);
+    characterWalk.front.imageMiddle.resize(75, 80);
+    characterWalk.front.imageLeftLeg.resize(75, 80);
+
+    characterWalk.back.imageRightLeg.resize(75, 80);
+    characterWalk.back.imageMiddle.resize(75, 80);
+    characterWalk.back.imageLeftLeg.resize(75, 80);
+
+    longHallway.floor.image.resize(150, 600);
 
 }
 
 function draw() {
+    background(gameBackdrop.image);
+    //  for (let characterFrontWalk of characterFrontWalks) {
+
+
+    // }
 
     drawLongHallway();
     drawCharacter();
     moveCharacter();
-    blockWallCharacter();
+    //  blockWallCharacter();
+
+}
+
+
+function moveCharacter() {
+    //  console.log(keyIsDown)
+
+    // if (!isMoving === false) {
+    //     characterWalk.imageFront = characterWalk.imageFront;
+    // }
+    isMoving = false;
+
+    if (keyIsDown(UP_ARROW)) {
+        isMoving = true;
+
+        characterWalk.y -= 2;
+
+        if (frameCount % 10 === 0) {
+            if (legPosition === 0) {
+                characterWalk.imageFront = characterWalk.back.imageRightLeg;
+                legPosition = 1;
+            }
+            else if (legPosition === 1) {
+                characterWalk.imageFront = characterWalk.back.imageMiddle;
+                legPosition = 2;
+            }
+            else if (legPosition === 2) {
+                characterWalk.imageFront = characterWalk.back.imageLeftLeg;
+                legPosition = 0;
+            }
+        }
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        isMoving = true;
+
+        characterWalk.y += 2;
+
+        if (frameCount % 10 === 0) {
+            if (legPosition === 0) {
+                characterWalk.imageFront = characterWalk.front.imageRightLeg;
+                legPosition = 1;
+            }
+            else if (legPosition === 1) {
+                characterWalk.imageFront = characterWalk.front.imageMiddle;
+                legPosition = 2;
+            }
+            else if (legPosition === 2) {
+                characterWalk.imageFront = characterWalk.front.imageLeftLeg;
+                legPosition = 0;
+            }
+        }
+    }
+
+    else {
+        isMoving = false;
+        characterWalk.imageFront = characterWalk.imageFront;
+
+    }
 
 }
 
@@ -95,19 +200,22 @@ function drawCharacter() {
     push();
     rectMode(CENTER);
     imageMode(CENTER);
-    rect(100, 100, characterStill.width, characterStill.height);
-    image(characterStill.image.front, 100, 100);
+    noFill();
+    rect(characterWalk.x, characterWalk.y, characterWalk.width, characterWalk.height);
+    image(characterWalk.imageFront, characterWalk.x, characterWalk.y);
     pop();
 
 }
 
 
 function drawLongHallway() {
+
+    //draw Floor
     push();
     //rectMode(CENTER);
     //imageMode(CENTER);
-    rect(300, 0, longHallway.width, longHallway.height);
-    image(longHallway.image, 300, 0);
+    rect(300, 0, longHallway.floor.width, longHallway.floor.height);
+    image(longHallway.floor.image, 300, 0);
     pop();
 
 }
