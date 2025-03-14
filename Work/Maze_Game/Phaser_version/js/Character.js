a61193d7286d84daa6e1e2738b3bcd766af11ac7
Biwanka <gauthier.bibi10@gmail.com>
2025-03-13 21:44:44 -0400

    close enough i got the character to move, weirdly the side imges are not switching and the character freezes when going up

--- Work/Maze_Game/Phaser_version/js/Character.js
+++ Work/Maze_Game/Phaser_version/js/Character.js
@@ -1,36 +1,24 @@
 class Character extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
-        super(scene, x, y, 'character', 0); // Set frame 0 (first frame of the spritesheet)
+        super(scene, x, y, 'character_front_left'); // Set frame 0 (first frame of the spritesheet)
 
         this.scene = scene;
-
-        // Debugging Logs
-        console.log("Character sprite:", this.texture.key);
-
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this);
-
         this.setCollideWorldBounds(true);
 
-        // Set size manually in case it's too small
-        this.setScale(2); // Try making it bigger
-        this.setSize(32, 32); // Set a size
+        this.setScale(0.2);
+        // // Add animations
+        // this.scene.anims.create({
+        //     key: 'walk',
+        //     frames: this.scene.anims.generateFrameNumbers('character', { start: 0, end: 3 }), // Adjust frame range
+        //     frameRate: 10,
+        //     repeat: -1
+        // });
 
-        // Add animations
-        this.scene.anims.create({
-            key: 'walk',
-            frames: this.scene.anims.generateFrameNumbers('character', { start: 0, end: 3 }), // Adjust frame range
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.play('walk'); // Play walk animation by default
+        // this.play('walk'); // Play walk animation by default
 
         // this.setAlpha(1); // Ensure it's not invisible
-
-        //    this.sprite = scene.physics.add.sprite(x, y, 'character');
-
-
 
         //Handle input keys
         this.keys = scene.input.keyboard.createCursorKeys();
@@ -41,48 +29,34 @@ class Character extends Phaser.Physics.Arcade.Sprite {
 
     update() {
 
-        console.log(`Character position: X=${this.x}, Y=${this.y}`);
+        //console.log(`Character position: X=${this.x}, Y=${this.y}`);
         this.setVelocity(0);
 
 
         if (this.keys.left.isDown) {
             this.setVelocityX(-this.speed);
+            this.play('walk_left', true);
         }
 
         else if (this.keys.right.isDown) {
             this.setVelocityX(this.speed);
+            this.play('walk_right', true);
         }
 
         if (this.keys.up.isDown) {
             this.setVelocityY(-this.speed);
+            this.play('walk_up', true);
         }
 
         else if (this.keys.down.isDown) {
             this.setVelocityY(this.speed);
+            this.play('walk_down', true);
+        }
+
+        else {
+            this.stop(); // Stop animation when no key is pressed
         }
     }
 }
 
 export default Character;
-
-// handleInput() {
-
-//     let speed = 150;
-//     this.sprite.setVelocity(0);
-
-//     if (this.keys.left.isDown) {
-//         this.sprite.setVelocityX(-speed);
-//     }
-
-//     else if (this.keys.right.isDown) {
-//         this.sprite.setVelocityX(speed);
-//     }
-
-//     if (this.keys.up.isDown) {
-//         this.sprite.setVelocityY(-speed);
-//     }
-
-//     else if (this.sprite.key.down.isDown) {
-//         this.sprite.setVelocityY(speed);
-//     }
-// }
